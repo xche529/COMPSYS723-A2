b9e0de66bf88a57c415d37aaa824450940dd20cf
Teresa-zzt <zzt020730@gmail.com>
2025-06-04 15:31:14 +1200

    update throttle cmd calculation code

--- ass2/produceConsume.h
+++ ass2/produceConsume.h
@@ -1,9 +1,6 @@
 //The type 'string' is defined as 'char*' in Esterel, hence passing a string by referenc  is 'char**'
 //Aviod naming a function 'recv'
-void send(int data);
-void recv1(char** data);
-void adding(unsigned int data);
-int remov();
-int outCount();
 int check_speed(float data);
 int check(float data);
+float saturateThrottle(float throttleIn, int* saturate);
+float regulateThrottle(int isGoingOn, float cruiseSpeed, float vehicleSpeed);
--- ass2/produceConsume_data.c
+++ ass2/produceConsume_data.c
@@ -9,13 +9,57 @@ int front=0;
 int rear=0;
 int cnt=0;
 
-int outCount(){
-  return cnt;
+/*
+DESCRIPTION: Saturate the throttle command to limit the acceleration.
+PARAMETERS: throttleIn - throttle input
+            saturate - true if saturated, false otherwise
+RETURNS: throttle output (ThrottleCmd)
+*/
+float saturateThrottle(float throttleIn, int* saturate)
+{
+	static const float THROTTLESATMAX = 45.0;
+	if (throttleIn > THROTTLESATMAX) {
+		*saturate = 1;
+		return THROTTLESATMAX;
+	}
+	else if (throttleIn < 0) {
+		*saturate = 1;
+		return 0;
+	}
+	else {
+		*saturate = 0;
+		return throttleIn;
+	}
 }
 
-void send(int data){
-	adding(data);
+/*
+DESCRIPTION: Saturate the throttle command to limit the acceleration.
+PARAMETERS: isGoingOn - true if the cruise control has just gone into the ON state 
+                        from another state; false otherwise
+            saturate - true if saturated, false otherwise
+RETURNS: throttle output (ThrottleCmd)
+*/
+float regulateThrottle(int isGoingOn, float cruiseSpeed, float vehicleSpeed)
+{
+	static const float KP = 8.113;
+	static const float KI = 0.5;
+	static int saturate = 1;
+	static float iterm = 0;
+	
+	if (isGoingOn==1) {
+		iterm = 0;	// reset the integral action
+		saturate = 1;	
+	}
+	float error = cruiseSpeed - vehicleSpeed;
+	float proportionalAction = error * KP;
+	if (saturate==1)
+		error = 0;	// integral action is hold when command is saturated
+	iterm = iterm + error;
+	float integralAction = KI * iterm;
+	return saturateThrottle(proportionalAction + integralAction, &saturate);
 }
+
+
 
 int check_speed(float data){
 	if (data<30 || data>150 ){
@@ -33,31 +77,6 @@ int check(float data){
 	}
 }
 
-void adding(unsigned int data){
-	if(cnt!=SIZE){
-		cq[rear]=data;
-		rear = (rear +1) % SIZE;
-		++cnt;
-	}
-}
 
-void recv1(char** data){
-	int tmp;
-	tmp = remov();
-	if(tmp != -1)
-		sprintf(data,"consumed %d",tmp);
-	else
-		sprintf(data,"consumed nothing");
-}
 
-int remov(void){
-	int val;
-	if(cnt!=0){
-		val=cq[front];
-		front = (front+1) % SIZE;
-		cnt--;
-		return val;
-	}else
-		return -1;
 
-}
